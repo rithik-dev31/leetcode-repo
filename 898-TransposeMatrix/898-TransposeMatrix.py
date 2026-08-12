06ae51146bfa741c5447b63bae76406966cad8e2
RITHIK E <rithik.devpro@gmail.com>
2026-08-12 11:28:30 +0530

    Create 898-TransposeMatrix

---
+++ 898-TransposeMatrix/898-TransposeMatrix.py
@@ -0,0 +1,12 @@
+# Last updated: 8/12/2026, 11:28:30 AM
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+
+        result = [[0] * m for _ in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                result[j][i] = matrix[i][j]
+
+        return result
