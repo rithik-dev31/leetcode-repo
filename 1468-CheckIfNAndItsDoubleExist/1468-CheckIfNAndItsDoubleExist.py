065cce79f5b9e4b5a3fb7cf2a35424348fcaa694
RITHIK E <rithik.devpro@gmail.com>
2026-08-12 11:27:37 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.py
@@ -0,0 +1,11 @@
+# Last updated: 8/12/2026, 11:27:37 AM
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+
+        for num in arr:
+            if num * 2 in seen or (num % 2 == 0 and num // 2 in seen):
+                return True
+            seen.add(num)
+
+        return False
